539ec547f40b4eba7c7598d5a739f64858b4d2ad
D'yara Williams <dyara.williams@gmail.com>
2024-10-14 00:47:41 -0400

    inital commit for part 1

---
+++ exercise1.py
@@ -0,0 +1,63 @@
+# Exercise 1-1
+# What is the function of the secondary memory in a computer?
+# a) Execute all of the computation and logic of the program
+# b) Retrieve web pages over the Internet
+# c) (answer) Store information for the long term, even beyond a power cycle
+# d) Take input from the user
+
+# Exercise 1-2
+# What is a program? 
+# A set of instructions that specifies a computation.
+
+# Exercise 1-3
+# What is the difference between a compiler and an interpreter?
+# A compiler translates a program written in a high-level language into a low-level language all at once, in preparation for later execution, and a interpreter executes a program in a high-level language by translating it one line at a time.
+
+# Exercise 1-4
+# Which of the following contains “machine code”?
+# a) The Python interpreter
+# b) The keyboard
+# c) (answer) Python source file
+# d) A word processing document
+
+# Exercise 1-5
+# What is wrong with the following code:
+# >>> primt 'Hello world!'
+# File "<stdin>", line 1
+#   primt 'Hello world!'
+#                      ^
+# SyntaxError: invalid syntax
+# >>>
+
+# Test
+# primt 'Hello world!'
+
+# The error that occurs is a syntax error "primt" was used when it should be "print"
+
+# Exercise 1-6
+# Where in the computer is a variable such as “x” stored after the following Python line finishes?
+# x = 123
+# a) Central processing unit
+# b) (answer) Main Memory
+# c) Secondary Memory
+# d) Input Devices
+# e) Output Devices
+
+# Exercise 1-7
+# What will the following program print out:
+x = 43
+x = x - 1
+print(x)
+# a) 43
+# b) (answer) 42
+# c) x + 1
+# d) Error because x = x + 1 is not possible mathematically
+
+# Exercise 1-8
+#  Explain each of the following using an example of a human capability: (1) Central processing unit, (2) Main Memory, (3) Secondary Memory, (4) Input Device, and (5) Output Device. For example, “What is the human equivalent to a Central Processing Unit”?
+
+# CPU - Heart of the computer
+# Main memory - Short term memory
+# Secondary Memory - Long term memory
+# Input Device - eating or drinking for your body
+# Output Device - ears for listening, eyes for seeing
